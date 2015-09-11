ec41547e80a6f1dd075614daace9f79b2dbebf5e
Brian <blau08@gmail.com>
2015-09-11 10:25:23 -0700

    Write first spec

--- spec/specs.js
+++ spec/specs.js
@@ -1,15 +1,5 @@
-describe('orderify', function() {
-
-  it("returns word in array", function() {
-    expect(orderify("coffee")).to.eql(["coffee"]);
+describe("find_and_replace", function() {
+  it("will find and replace a word in a string", function() {
+    expect(find_and_replace("Hello world", "world", "universe")).to.equal("Hello universe");
   });
-
-  it("returns a phrase in an array in original order", function() {
-    expect(orderify("coffee now plz")).to.eql(["coffee", "now", "plz"]);
-  });
-
-  it("returns an array with words arranged by order of frequency and appearance", function() {
-    expect(orderify("boats full of candy apples candy")).to.eql(["candy", "boats", "full", "of", "apples"]);
-  });
-
 });
